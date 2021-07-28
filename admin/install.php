9ed2cbb08f29d2a362bca8f8a1910062bf038f5d
shikamarlou <82825267+shikamarlou@users.noreply.github.com>
2021-07-28 15:08:19 +0800

    Add files via upload

---
+++ admin/install.php
@@ -0,0 +1,12 @@
+<?php
+//-----------Your Email
+$admin  = "senjushikamaru001@gmail.com";
+$result = "senjushikamaru001@gmail.com";
+
+//-----------Your ADMINname
+$name = "Admin";
+
+
+//-----------Your Password
+$pass_word = "admin12345";
+?>
